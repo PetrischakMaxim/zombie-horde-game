b4eda8e5aa43259ecb54ffa284a23dfb5fc8ef3e
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-04-20 18:22:19 +0300

    feat: add state

--- src/components/spawner.ts
+++ src/components/spawner.ts
@@ -1,25 +1,22 @@
 import Zombie from "./zombie";
-import {Application} from "pixi.js";
-import {State} from "./state";
+import Game from "../components/game";
+import State from "./state";
 
 export default class Spawner {
     _time: number;
     _maxCount: number;
     _callback: Function;
-    _app: Application;
-    _state: string;
+    _app: Game;
 
     public children: Array<Zombie>;
 
     constructor(options: {
-        app: Application,
-        state: State,
+        app: Game,
         callback: Function,
     }) {
         this._time = 2500;
         this._maxCount = 10;
         this._app = options.app;
-        this._state = options.state;
         this._callback = options.callback;
         this.children = [];
         this._generate();
@@ -27,16 +24,13 @@ export default class Spawner {
 
     private _generate() {
         window.setInterval(() => {
-            if (this._state.RUNNING) {
-                this._spawn()
-            }
+            this._spawn()
         }, this._time);
     }
 
     private _spawn() {
-        if (this.children.length >= this._maxCount) {
-            return;
-        }
+        if (this._app.state !== State.RUNNING) return;
+        if (this.children.length >= this._maxCount) return;
         const zombie = this._callback();
         this.children.push(zombie);
     }
