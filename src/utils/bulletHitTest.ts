fdf9b4e720407ed0ca662c530d49377f48dacd9b
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-04-20 18:18:45 +0300

    feat: add removing active bullet

--- src/utils/bulletHitTest.ts
+++ src/utils/bulletHitTest.ts
@@ -8,8 +8,9 @@ export function bulletHitTest(bullets: Array<Bullet>, zombies: Array<Zombie>, bu
             const dy = zombie.position.y - bullet.position.y;
             const distance = Math.sqrt(dx * dx + dy * dy);
             if (distance < bulletRadius + zombieRadius) {
+                bullets.splice(index, 1);
+                bullet.alpha = 0;
                 zombies.splice(index, 1);
-
                 zombie.kill();
             }
         });
