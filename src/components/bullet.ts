e575a0e8d2a710a9d182539733d7cb7ec02ed647
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-04-20 18:13:05 +0300

    feat: add properties

--- src/components/bullet.ts
+++ src/components/bullet.ts
@@ -1,12 +1,17 @@
 import {Sprite,Loader} from "pixi.js";
 import Victor from "victor";
+import Player from "./player";
 
 export default class Bullet extends Sprite {
     public velocity: Victor;
 
-    constructor(texture = Loader.shared.resources["bullet"].texture) {
+    constructor(player: Player, texture = Loader.shared.resources["bullet"].texture) {
         super();
-        this.texture = texture;
+        this.texture = Loader.shared.resources["bullet"].texture;
+        this.anchor.set(0.5);
+        this.scale.set(0.2);
+        this.position.set(player.position.x, player.position.y);
+        this.rotation = player.rotation;
         this.velocity = null;
     }
 }
