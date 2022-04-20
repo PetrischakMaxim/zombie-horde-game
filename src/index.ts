8847d04fee47d92342711936bf3ae96e5d792b84
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-04-20 18:27:58 +0300

    refactor: update file

--- src/index.ts
+++ src/index.ts
@@ -1,107 +1,82 @@
-import {Application, SCALE_MODES, settings} from "pixi.js";
-import {bulletHitTest} from "./utils/bulletHitTest";
+import { SCALE_MODES, settings} from "pixi.js";
 import Player from "./components/player";
 import Zombie from "./components/zombie";
 import Spawner from "./components/spawner";
 import Weather from "./components/weather";
-import {State} from "./components/state";
-import loadAssets from "./utils/loadAssets";
-import createScene from "./utils/createScene";
+import State from "./components/state";
+import Game from "./components/game";
+import {loadAssets} from "./utils/loadAssets";
+import {createScene} from "./utils/createScene";
+import {initSound} from "./utils/initSound";
+import {assetsPath, gameOptions, sceneText} from "./utils/constants";
+import {bulletHitTest} from "./utils/bulletHitTest";
 
-const canvasSize = 512;
-const canvasElement = document.querySelector('#root') as HTMLCanvasElement;
-
-const app = new Application ({
-    view: canvasElement as HTMLCanvasElement,
-    width: canvasSize,
-    height: canvasSize,
-    backgroundColor: 0x312a2b,
-    resolution: 2
-});
-
+const game = new Game(gameOptions);
 settings.SCALE_MODE = SCALE_MODES.NEAREST;
 
-const gameState = State.PREINTRO;
-const introScene = createScene(app,"HordeZee", "Click to Continue");
-const startScene = createScene(app,"HordeZee", "Click to Start");
-const endScene = createScene(app,"HordeZee", "Game Over");
-
+initGame();
 
 async function initGame() {
+    game.state = State.PREINTRO;
     try {
         await loadAssets();
-
-        // Todo Refactor
-        const player = new Player(app);
+        game.weather = new Weather(game);
+        const player = new Player(game);
         const spawner = new Spawner({
-            app: app,
-            state: gameState,
-            callback: () => new Zombie({app, player})
+            app: game,
+            callback: () => new Zombie({ app: game, player })
         });
 
-        app.ticker.add((delta) => runGame(delta));
+        const gamePreIntroScene = createScene(game,sceneText.title, sceneText.intro);
+        const gameStartScene = createScene(game,sceneText.title, sceneText.start);
+        const gameOverScene = createScene(game,sceneText.title, sceneText.end);
 
-        const runGame = (delta: number) => {
-            if(player.isDead) gameState = State.GAMEOVER;
-            introScene.visible = gameState.PREINTRO;
-            startScene.visible = gameState.START;
-            endScene.visible = gameState.GAMEOVER;
-
-            switch (gameState) {
-                case gameState.PREINTRO:
+        game.ticker.add((delta) => {
+            if (player.isDead) game.state = State.GAMEOVER;
+            gamePreIntroScene.visible = game.state === State.PREINTRO;
+            gameStartScene.visible = game.state === State.START;
+            gameOverScene.visible = game.state === State.GAMEOVER;
+            switch (game.state) {
+                case State.PREINTRO:
                     player.scale = 4;
                     break;
-                case gameState.INTRO:
-                    player.scale -= 0.01;
-                    if (player.scale <= 1) gameState = gameState.START;
+                case State.INTRO:
+                    player.scale = 1;
+                    // if (player.scale <= 1)
+                    game.state = State.START;
                     break;
-                case gameState.RUNNING:
+                case State.RUNNING:
                     player.update(delta);
-                    spawner.children.forEach((zombie: Zombie) => zombie.update(delta));
-
-                    bulletHitTest(
-                        player.shooting.bullets,
-                        spawner.children,
-                        8,
-                        16,
-                    );
+                    spawner.children.forEach((zombie) => zombie.update(delta));
+                    bulletHitTest(player.shooting.bullets, spawner.children, 8,16);
                     break;
                 default:
                     break;
             }
-        }
-
-        // End refactor
+        });
     } catch (error) {
         console.log(error.message);
+        console.log("Load failed");
     }
 }
 
-function clickHandler() {
+function onDocumentClick() {
+    const gameMusic = initSound(`${assetsPath}/sounds/HordeZee.mp3`);
+    const zombieHordeSound = initSound(`${assetsPath}/sounds/horde.mp3`);
 
-    switch (gameState) {
-        case gameState.PREINTRO:
-            gameState = gameState.INTRO;
-            // music.play();
-            // weather.enableSound();
+    switch (game.state) {
+        case State.PREINTRO:
+            game.state = State.INTRO;
+            gameMusic.play();
+            game.weather.enableSound();
             break;
-        case gameState.START:
-            gameState = gameState.RUNNING;
-            // zombieHorde.play();
+        case State.START:
+            game.state = State.RUNNING;
+            zombieHordeSound.play();
             break;
         default:
             break;
     }
 }
 
-initGame()
-    .then(() => {
-        document.addEventListener("click", clickHandler, {once: true});
-    });
-
-
-
-
-
-
-
+document.addEventListener("click", onDocumentClick);
