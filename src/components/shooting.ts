6d06374927a177d9f63fc367f0f7f40e830a3868
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-04-20 18:21:24 +0300

    refactor: fix shooting bug, update class

--- src/components/shooting.ts
+++ src/components/shooting.ts
@@ -1,7 +1,8 @@
 import {Application} from "pixi.js";
 import Victor from "victor";
-import Bullet from "./bullet";
 import Player from "./player";
+import Bullet from "./bullet";
+import {assetsPath} from "../utils/constants";
 
 interface Options {
     app: Application,
@@ -14,56 +15,58 @@ export default class Shooting {
     _speed: number;
     _maxBullets: number;
     _timerId: number;
+    _bulletRadius: number;
+    _sound: HTMLAudioElement;
 
     constructor(options: Options) {
         this._options = options;
         this._speed = 4;
         this._bullets = [];
         this._maxBullets = 3;
+        this._bulletRadius = 8;
+        this._sound = new Audio(`${assetsPath}/sounds/shoot.mp3`);
+    }
+
+    _playSound() {
+        this._sound.currentTime = 0;
+        this._sound.play();
     }
 
     _fire() {
-        if(this._bullets.length) {
-            this._checkBullets();
-        }
+        this._playSound();
+        const {stage} = this._options.app;
 
-        this._renderBullet(new Bullet());
-    }
-
-    _renderBullet(bullet: Bullet) {
-        bullet.anchor.set(0.5);
-        bullet.scale.set(0.2);
-        bullet.position.set(
-            this._options.player.position.x,
-            this._options.player.position.y
-        );
-        bullet.rotation = this._options.player.rotation;
-
-        const angle = this._options.player.rotation - Math.PI / 2;
-
-        bullet.velocity = new Victor(
-            Math.cos(angle),
-            Math.sign(angle)
-        ).multiplyScalar(this._speed);
-
-        this._bullets.push(bullet);
-        this._options.app.stage.addChild(bullet);
-    }
-
-    _checkBullets() {
         if (this._bullets.length >= this._maxBullets) {
-            const bullet = this._bullets.shift();
-            this._options.app.stage.removeChild(bullet);
+            let b = this._bullets.shift();
+            stage.removeChild(b);
         }
 
-        this._bullets.forEach(bullet => this._options.app.stage.removeChild(bullet));
-        this._bullets = this._bullets.filter(
-            (bullet) =>
-                Math.abs(bullet.position.x) < this._options.app.screen.width &&
-                Math.abs(bullet.position.y) < this._options.app.screen.height
+        this._bullets.forEach((b) => stage.removeChild(b));
+        this._bullets = this._bullets.filter((bullet) =>
+            Math.abs(bullet.position.x) < this._options.app.screen.width &&
+            Math.abs(bullet.position.y) < this._options.app.screen.height
         );
+        this._bullets.forEach((b) => stage.addChild(b));
 
-        this._bullets.forEach(bullet => this._options.app.stage.addChild(bullet));
+        const bullet = this._setupBullet(this._options);
+        this._bullets.push(bullet);
+        stage.addChild(bullet);
+    }
+
+    _setupBullet({player} = this._options) {
+        const bullet = new Bullet(player);
+        const angle = player.rotation - Math.PI / 2;
+        bullet.velocity = new Victor(Math.cos(angle), Math.sin(angle)).multiplyScalar(this._speed);
+        return bullet;
+    }
+
+    set shoot(shooting: boolean) {
+        if (shooting) {
+            this._fire();
+            this._timerId = window.setInterval( () => this._fire(), 500);
+        } else {
+            window.clearInterval(this._timerId);
+        }
     }
 
     public update(delta: number) {
@@ -73,15 +76,6 @@ export default class Shooting {
                 bullet.position.y + bullet.velocity.y * delta
             )
         );
-    }
-
-    set shoot(shooting: boolean) {
-        if (shooting) {
-            this._fire();
-            this._timerId = window.setInterval(this._fire, 500);
-        } else {
-            window.clearInterval(this._timerId);
-        }
     }
 
     get bullets() {
