f719ffbbdae44954231f5d6ca20e8201c20d6d15
Maxim Petrischak <petrischak.maxim@gmail.com>
2022-04-21 16:21:38 +0300

    refactor: update code

--- src/components/game.ts
+++ src/components/game.ts
@@ -1,7 +1,15 @@
 import {Application} from "pixi.js";
+import {IApplicationOptions} from "@pixi/app";
 import State from "./state";
 import Weather from "./weather";
-import {IApplicationOptions} from "@pixi/app";
+import Player from "./player";
+import Spawner from "./spawner";
+import Zombie from "./zombie";
+import {assetsPath, sceneText} from "../utils/constants";
+import {loadAssets} from "../utils/loadAssets";
+import {createScene} from "../utils/createScene";
+import {bulletHitTest} from "../utils/bulletHitTest";
+import {initSound} from "../utils/initSound";
 
 export default class Game extends Application {
     public state: State;
@@ -11,5 +19,67 @@ export default class Game extends Application {
         super(props);
         this.state = null;
         this.weather = null;
+    }
+
+    public async init() {
+        this.state = State.PREINTRO;
+        try {
+            await loadAssets();
+            this.weather = new Weather(this);
+            const player = new Player(this);
+            const spawner = new Spawner({
+                app: this,
+                callback: () => new Zombie({ app: this, player })
+            });
+
+            const gamePreIntroScene = createScene(this,sceneText.title, sceneText.intro);
+            const gameStartScene = createScene(this,sceneText.title, sceneText.start);
+            const gameOverScene = createScene(this,sceneText.title, sceneText.end);
+
+            this.ticker.add((delta) => {
+                if (player.isDead) this.state = State.GAMEOVER;
+                gamePreIntroScene.visible = this.state === State.PREINTRO;
+                gameStartScene.visible = this.state === State.START;
+                gameOverScene.visible = this.state === State.GAMEOVER;
+                switch (this.state) {
+                    case State.PREINTRO:
+                        player.scale = 4;
+                        break;
+                    case State.INTRO:
+                        player.scale = 1;
+                        this.state = State.START;
+                        break;
+                    case State.RUNNING:
+                        player.update(delta);
+                        spawner.children.forEach((zombie) => zombie.update(delta));
+                        bulletHitTest(player.shooting.bullets, spawner.children, 8,16);
+                        break;
+                    default:
+                        break;
+                }
+            });
+        } catch (error) {
+            console.log(error.message);
+            console.log("Load failed");
+        }
+    }
+
+    public run() {
+        const gameMusic = initSound(`${assetsPath}/sounds/HordeZee.mp3`);
+        const zombieHordeSound = initSound(`${assetsPath}/sounds/horde.mp3`);
+
+        switch (this.state) {
+            case State.PREINTRO:
+                this.state = State.INTRO;
+                gameMusic.play();
+                this.weather.enableSound();
+                break;
+            case State.START:
+                this.state = State.RUNNING;
+                zombieHordeSound.play();
+                break;
+            default:
+                break;
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,9 @@
 import { SCALE_MODES, settings} from "pixi.js";
-import Player from "./components/player";
-import Zombie from "./components/zombie";
-import Spawner from "./components/spawner";
-import Weather from "./components/weather";
-import State from "./components/state";
 import Game from "./components/game";
-import {loadAssets} from "./utils/loadAssets";
-import {createScene} from "./utils/createScene";
-import {initSound} from "./utils/initSound";
-import {assetsPath, gameOptions, sceneText} from "./utils/constants";
-import {bulletHitTest} from "./utils/bulletHitTest";
+import {gameOptions} from "./utils/constants";
 
-const game = new Game(gameOptions);
 settings.SCALE_MODE = SCALE_MODES.NEAREST;
+const game = new Game(gameOptions);
+game.init()
+    .then(() => document.addEventListener("click", () => game.run()));
 
-initGame();
-
-async function initGame() {
-    game.state = State.PREINTRO;
-    try {
-        await loadAssets();
-        game.weather = new Weather(game);
-        const player = new Player(game);
-        const spawner = new Spawner({
-            app: game,
-            callback: () => new Zombie({ app: game, player })
-        });
-
-        const gamePreIntroScene = createScene(game,sceneText.title, sceneText.intro);
-        const gameStartScene = createScene(game,sceneText.title, sceneText.start);
-        const gameOverScene = createScene(game,sceneText.title, sceneText.end);
-
-        game.ticker.add((delta) => {
-            if (player.isDead) game.state = State.GAMEOVER;
-            gamePreIntroScene.visible = game.state === State.PREINTRO;
-            gameStartScene.visible = game.state === State.START;
-            gameOverScene.visible = game.state === State.GAMEOVER;
-            switch (game.state) {
-                case State.PREINTRO:
-                    player.scale = 4;
-                    break;
-                case State.INTRO:
-                    player.scale = 1;
-                    // if (player.scale <= 1)
-                    game.state = State.START;
-                    break;
-                case State.RUNNING:
-                    player.update(delta);
-                    spawner.children.forEach((zombie) => zombie.update(delta));
-                    bulletHitTest(player.shooting.bullets, spawner.children, 8,16);
-                    break;
-                default:
-                    break;
-            }
-        });
-    } catch (error) {
-        console.log(error.message);
-        console.log("Load failed");
-    }
-}
-
-function onDocumentClick() {
-    const gameMusic = initSound(`${assetsPath}/sounds/HordeZee.mp3`);
-    const zombieHordeSound = initSound(`${assetsPath}/sounds/horde.mp3`);
-
-    switch (game.state) {
-        case State.PREINTRO:
-            game.state = State.INTRO;
-            gameMusic.play();
-            game.weather.enableSound();
-            break;
-        case State.START:
-            game.state = State.RUNNING;
-            zombieHordeSound.play();
-            break;
-        default:
-            break;
-    }
-}
-
-document.addEventListener("click", onDocumentClick);
